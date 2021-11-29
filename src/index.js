e34896973cd35b87415d70c2a7d37f509ef0601f
danielj0nes <daniel@jones.ac>
2021-11-29 20:53:21 +0100

    Should be completely working as intended on OSX now

--- src/index.js
+++ src/index.js
@@ -85,8 +85,7 @@ ipcMain.on("startBreakbuddy", function (evt, message) {
         tray = new Tray(iconPath);
         tray.setToolTip("BreakBuddy");
         tray.setContextMenu(contextMenu);
-        if (process.platform == "darwin") console.log("Check")
-        else BrowserWindow.getAllWindows()[1].close();
+        BrowserWindow.getAllWindows()[1].close();
         
         // Start logging movements to track idle time (i.e., if break taken)
         // This doesn't work on certain applications (so far only LoL client...)
